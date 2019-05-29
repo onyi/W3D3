fb1ff9d2e530b2b6cbef67b4e7f57a57504cec81
onyi <onyicho@gmail.com>
2019-05-29 15:51:29 -0700

    Fix config for non-required column to prevent error by adding optional param in Course model

--- associations/app/models/course.rb
+++ associations/app/models/course.rb
@@ -20,7 +20,8 @@ class Course < ApplicationRecord
   belongs_to :prerequisite,
     primary_key: :id,
     foreign_key: :prereq_id,
-    class_name: :Course
+    class_name: :Course,
+    optional: true
   
   belongs_to :instructor,
     primary_key: :id,
